3aa36944eea8c08680492619ace7e891180608c7
theresamm <mtheresamayo@gmail.com>
2022-08-16 14:42:55 +0000

    initial commit

--- MongoUtil.js
+++ MongoUtil.js
@@ -1,6 +1,5 @@
 const mongodb = require('mongodb');
 function connect(){
-    const client = mongodb.connect("mongodb+srv://user:DRiueeqDbJLvXSPp@cluster1.4ilix.mongodb.net/?retryWrites=true&w=majority",{
-        useUnifiedTopology: true
+     useUnifiedTopology: true
         })
 }
