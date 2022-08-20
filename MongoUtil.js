b49d1758e87dfcc8e3ecb8a5817798438c3cc3ad
theresamm <mtheresamayo@gmail.com>
2022-08-20 12:46:41 +0000

    Added new review and env file

--- MongoUtil.js
+++ MongoUtil.js
@@ -1,5 +1,9 @@
-const mongodb = require('mongodb');
-function connect(){
-     useUnifiedTopology: true
+const MongoClient = require('mongodb').MongoClient;
+async function connect(mongoUri, databasename){
+        const client = await MongoClient.connect(mongoUri,{
+        useUnifiedTopology: true
         })
+        const db = client.db(databasename);
+        return db;
 }
+module.exports = {connect}
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const mongoUtil = require('./MongoUtil');
+
 const app = express();
 app.use(express.json())
 app.use(cors());
-app.get('/', function(req,res){
-    res.json({
-        'text':'hello'
-    });
-})
+
+const MONGOURI = process.env.MONGOURI;
+const DBNAME = process.env.DBNAME;
+
+async function main(){
+    const db = await mongoUtil.connect(MONGOURI, DBNAME);
+    app.get('/', function(req,res){
+        res.json({
+            'text':'hello'
+        });
+    })
+    app.post('/restaurantreviews', async function (req,res){
+        await db.collection('restaurantreviews').insertOne({
+            "name":"Oishi Restaurant",
+            "cuisine":"Japanese",
+            "location":"Manila",
+            "bestseller":"Tempura",
+            "rating":5
+        })
+        res.json({
+            'message':'done'
+        })
+    })
+}
+main();
+
 app.listen(3000,function(){
     console.log("server has started")
 })
